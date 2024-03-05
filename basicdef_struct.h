2ef9fa12cee8a8e0317008da4eff0b11002e84c2
Tau0509 <138314426+Tau0509@users.noreply.github.com>
2024-03-05 20:28:08 +0800

    一些声明

---
+++ basicdef_struct.h
@@ -0,0 +1,9 @@
+//
+//对于数据结构中的库函数、参量进行声明
+// Created by 陶子昊 on 2024/3/5.
+//
+
+#ifndef DATASTRUCT_C_BASICDEF_STRUCT_H
+#define DATASTRUCT_C_BASICDEF_STRUCT_H
+
+#endif //DATASTRUCT_C_BASICDEF_STRUCT_H
